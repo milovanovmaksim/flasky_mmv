b6ac9edbe9d7026b587b3027320d7fab56e31462
maxim <milovanov160386@gmail.com>
2021-05-18 00:13:30 +0300

    обновил werkzeug

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from flask_script import Manager, Shell
 from app import create_app, db
 from app.models import User, Role, Permission, Post
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'heroku')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
