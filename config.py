c4e2d7c2e304802c9de88d512913cc31c026302b
maxim <milovanov160386@gmail.com>
2021-05-22 15:15:40 +0300

    изменил настройки в config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     SQLALCHEMY_RECORD_QUERIES = True
 
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
